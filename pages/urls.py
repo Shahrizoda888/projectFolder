561cfa7eaa17b2f4298669337085321ad1e62e67
shahrizodasub <shahrizodasubhonova2006@gmail.com>
2021-07-29 11:10:12 +0500

    change to templates

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BlogListView
+from .views import BlogListView,BlogDetailView
 
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 class BlogListView(ListView):
     model = Post
     template_name='home.html'
 
-# Create your views here.
+class BlogDetailView(DetailView):
+    model = Post
+    template_name='post_detail.html'
 
 
 
